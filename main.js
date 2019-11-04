8e70f22dd9978405afd0db5db948d207811f00c1
Martyna <martyna.b@hotmail.com>
2019-11-04 00:57:53 -0800

    "initial commit" :wq

---
+++ main.js
@@ -0,0 +1,112 @@
+const STORE = [
+    {
+			question: "Who were the original Avengers in the comic book?",
+			answers: [
+				"Batman, Spider-Man, Superman and Wonder Woman",
+				"Ant-Man, the Hulk, Iron Man, Thor, and the Wasp",
+				"The Hulk, Howard The Duck, Thor and Black Widow",
+				"Thor, Capitan America, Iron Man’, Black Widow",
+				"Fantastic 4"
+			],
+			correct: "Ant-Man, the Hulk, Iron Man, Thor, and the Wasp"
+		},
+		{
+			question: "Who wrote the Avengers?",
+			answers: [
+				"Bruce Banner",
+				"Stan Lee",
+				"J.K. Rawling",
+				"Gardner Fox",
+				"Tim Burton"
+			],
+			correct: "Stan Lee"
+		},
+		{
+			question: "Where was the Avengers headquarters?",
+			answers: [
+				"Paris",
+				"Gotham City",
+				"Hogwarts",
+				"New York",
+				"San Francisco"
+			],
+			correct: "New York"
+		},
+		{
+			question: "From which Universe are the Avengers?",
+			answers: [
+				"DC",
+				"Milky Way Galaxy",
+				"Marvel",
+				"Universal studio",
+				"Wizarding World"
+			],
+			correct: "Marvel"
+		},
+		{
+			question: "Who was never a part of the Avengers?",
+			answers: [
+				"Spiderman",
+				"Jean Grey",
+				"Deadpool",
+				"Wolverine",
+				"Black Widow",
+			],
+            correct: "Jean Grey"
+		},
+		{
+			question: "What was the name of Tony Stark’s butler?",
+			answers: [
+				"Jughead",
+				"Bruce",
+				"Dobby",
+				"Jarvis",
+				"Thanos"
+			],
+			correct: "Jarvis"
+		},
+		{
+			question: "Which original Avengers die at the end of the movie adaptation 'Endgame'?",
+			answers: [
+				"Wasp, Deadpool, Jarvis",
+				"Spiderman, Doctor Strange, Hulk",
+				"Black Widow, Hawkeye",
+				"Iron Man, Starlord, Spiderman",
+				"Black Widow, Iron Man, Capitan America"
+			],
+			correct: "Black Widow, Iron Man, Capitan America"
+		},
+		{
+			question: "What is Black Widow’s name?",
+			answers: [
+				"Jessica Jones",
+				"Hermione Granger",
+				"Natalia Alianovna Romanoff",
+				"Scarlett Johansson",
+				"Jean Grey"
+				],
+			correct: "Natalia Alianovna Romanoff"
+		},
+		{
+			question: "How many Avengers movies are there?",
+			answers: [
+				"5",
+				"7",
+				"3",
+				"4",
+				"10"
+			],
+			correct: "4"
+		},
+		{
+			question: "What is the title of the second Avengers Movie?",
+			answers: [
+				"The Avengers",
+				"Avengers: Age of Ultron",
+				"Avengers: Infinity War",
+				"Avengers: The Chamber of Secrets",
+				"Avengers: Hichhiker’s Guide to the Universe"
+			],
+			correct: "Avengers: Age of Ultron",
+		}
+		];
