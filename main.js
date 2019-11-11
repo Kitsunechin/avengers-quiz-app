9912f88c29eb7727a3aa5e488da1450a1ef21bba
Martyna <martyna.b@hotmail.com>
2019-11-10 21:37:43 -0800

    "adding js file, cleaned up html, css updated, assets file"

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const STORE = [
 				"Batman, Spider-Man, Superman and Wonder Woman",
 				"Ant-Man, the Hulk, Iron Man, Thor, and the Wasp",
 				"The Hulk, Howard The Duck, Thor and Black Widow",
-				"Thor, Capitan America, Iron Man’, Black Widow",
+				"Thor, Capitan America, Iron Man, Black Widow",
 				"Fantastic 4"
 			],
 			correctAnswer: 1
@@ -110,134 +110,174 @@ const STORE = [
 			correctAnswer: 1
 		}
 		];
-
 		// set counter variables for score and question numbers to 0 
 		let score = 0;
-		let questionNum = 0;
-		let index = 0;
-
+		let questionNum = 1;
+		let index = -1;
 		// load the intro screen
 		function displayIntro() {
-		$("#js-intro").show;
-		$("#js-question").hide;
-		$("#js-feedback").hide;
-		$("#js-summary").hide;
-		}
-
+			$("#js-intro").show();
+			$("#js-question").hide();
+			$("#js-feedback").hide();
+			$("#js-summary").hide();
+		};
 		// bind an event to the start button and trigger the function to 
 		// display the screen with questions
 		$("#js-intro").on("click", ".js-start", event => {
 			event.preventDefault();
 			displayQuestion();
-		})
-
+		});
 		// update the score counter
 		function updateScore() {
 			score ++;
-			$(".js-score").text(score);
+			$("span.js-score").text(`${score}`);
 		};
-
 		// update the question counter
-		function updateQuestionNum(){
-			if (questionNum!==10) {questionNum ++};
-			$(".js-question-num").text(questionNum - 1);
+		function updateQuestionNum() {
+			questionNum ++;		
+			$(".js-question-num").text(`${questionNum}`);
 		};
-
+		// update question index of the element in STORE
 		function increaseIndex() {
+			// if (index <= STORE.length){
 			index ++;
-		  };
-
+		};
+		// show question box only 
 		function showQuestion() {
-		$("#js-intro").hide;
-		$("#js-question").show;
-		$("#js-feedback").hide;
-		$("#js-summary").hide;
-		}
-
+			$("#js-intro").hide();
+			$("#js-question").show();
+			$("#js-feedback").hide();
+			$("#js-summary").hide();
+		};
 		// generate and populate dynamically the form with the elements stored in the variable
-		function displayQuestion(){
-			updateQuestionNum()
+		function displayQuestion() {
 			increaseIndex();
 			showQuestion();
-			$("#js-question").html(`<form role="form" action="/destination_page" accept-charset="UTF-8" class="quiz-box" method="POST"></form>
-			<section class="display-result js-counters">
-					<ul>
-						<li class="counter-q">Question:
-							<span class="js-question-num">${questionNum}</span>/10</li>
-						<li class="counter-s">Score:
-							<span class="js-score">${score}</span>
-						</li>
-					</ul>
-			</section>
-			<section class="question-box">
+			$(".js-question-box").html(`<form role="form" action="/destination_page" accept-charset="UTF-8" class="quiz-box" method="POST">
 				<fieldset>
-						<legend>${STORE[index-1].question}</legend>
-						<label for="option-0" name="answer-label">${STORE[index-1].answers[0]}</label>
-						<input type="radio" name="answer" value="0" required><br>
-						<label for="option-1" name="answer-label">${STORE[index-1].answers[1]}</label>
-						<input type="radio" name="answer" value="1" required><br>
-						<label for="option-2" name="answer-label">${STORE[index-1].answers[2]}</label>
-						<input type="radio" name="answer" value="2" required><br>
-						<label for="option-3" name="answer-label">${STORE[index-1].answers[3]}</label>
-						<input type="radio" name="answer" value="3" required><br>
-						<label for="option-4" name="answer-label">${STORE[index-1].answers[4]}</label>
-						<input type="radio" name="answer" value="4" required><br>
+					<div class="pic-opacity"></div>
+					<legend>${STORE[index].question}</legend>
+						<label name="answer-label">${STORE[index].answers[0]}
+						<input type="radio" name="answer" value="0"></label>
+						<label name="answer-label">${STORE[index].answers[1]}
+						<input type="radio" name="answer" value="1"></label>
+						<label name="answer-label">${STORE[index].answers[2]}
+						<input type="radio" name="answer" value="2"></label>
+						<label name="answer-label">${STORE[index].answers[3]}
+						<input type="radio" name="answer" value="3"></label>
+						<label name="answer-label">${STORE[index].answers[4]}
+						<input type="radio" name="answer" value="4"></label>
 						<button type="submit" class="submit-answer">Submit</button>
-				</fieldset> </form>
-			</section>`)
-		}
+				</fieldset>
+				</form>`);
+		};
 		// bind an evnt on a click to listen for a submit to get the 'checked' value of the radio button
 		// call the function comparing that value against the value of the correct answer
 		// call the function for feedback
 		// update page counter
 		function submitAnswer() {
-			let submitResult = $("#js-question").on("click", ".submit-answer", event => {
+			$("#js-question").on("click", ".submit-answer", event => {
 				event.preventDefault();
-				let getValue = $(".question-box").find("input[type=radio][name=answer]:checked").val();
+				let checkedOption = $(".js-question-box").find("input[type=radio][name=answer]:checked");
+				let getValue = $(".js-question-box").find("input[type=radio][name=answer]:checked").val();
 				let correctAnswer = `${STORE[index].correctAnswer}`;
 				let updateAnswer = $("#js-feedback");
-				if(getValue === correctAnswer){
+				if (!checkedOption.is(":checked")) {
+					alert("You have to choose an option")
+				} else if (getValue === correctAnswer) {
 					updateScore();
-					updateAnswer.append(`<section class="feedback-box">
-					<img alt="Happy Groot Image"></img>
-					<p>You guessed right!</p>
-					<button type="submit" class="next-question js-next">Next</button>
-				  </section>`);
-				} else {
-					updateAnswer.append(`<section class="feedback-box">
-					<img alt="Happy Thanos Image"></img>
+					updateQuestionNum();
+					lastQuestion();
+					displayFeedback();
+					updateAnswer.html(`
+					<section class="feedback-box">
+						<p>You guessed right!</p>
+						<img class="image" src="Pictures/correct.jpg" alt="Happy Groot Image"></img>
+						<button type="submit" class="submit-answer" id="js-next"">Next</button>
+					</section>`);
+				} else if (getValue !== correctAnswer) {
+					updateQuestionNum();
+					lastQuestion();
+					displayFeedback();
+					updateAnswer.html(`
+					<section class="feedback-box">
 					<p>Sorry, this is not the correct answer</p>
 					<p>The correct answer is: ${STORE[index].answers[correctAnswer]}</p>
-					<button type="submit" class="next-question js-next">Next</button>
-				</section>`)
-				};
+						<img class="image" src="Pictures/incorrect.jpeg" alt="Happy Thanos Image"></img>
+						<button type="submit" class="submit-answer" id="js-next">Next</button>
+					</section>`)
+				}
 				return updateAnswer;	
 			});
-			return submitResult;
+		// return submitResult;
 		};
-
 		// write a function displaying feedback // displayFeedback();
 		// if correct display "you are right" feedback + display counters function
 		// if incorrect display "you are wrong" feedback + display updated counters function
-
+		function displayFeedback() {
+			$("#js-question").hide();
+			$("#js-feedback").show();
+			nextQuestion();
+		};
 		// write event listener on next button and call the next question and update the question counter
 		// if question is nr 10 call function for summary if not then display next question
-
-		// write summary function and display final score // displaySummary();
+		function nextQuestion() {
+			$("#js-feedback").on("click", "#js-next", event => {
+				event.preventDefault();
+				if (questionNum === 11) {
+					showSummary();
+				} else {
+					showQuestion();
+				}
+			});
+		};
+		//show Summary
+		function showSummary() {
+			$("#js-summary").show();
+			$("#js-feedback").hide();
+			$("#js-question").hide();
+		}
+		// write a summary function and display final score // displaySummary();
+		function displaySummary() {
+			$("#js-summary").html(`<section class="summary-box">
+			<p>Your final score is: <span class="js-final-score">${score}</span> out of 10</p>
+            <img class="image" src="Pictures/summary.jpg" alt="Doctor Strange Image"></img>
+            <button type="submit" class="restart-quiz id="js-restart">Restart</button>
+        </section>`);
+		};
+		// If last question
+		function lastQuestion() {
+			if (questionNum === 11) {
+				displaySummary();
+			} else {
+				displayQuestion();
+			}
+		};
 		
 		// bind event on a click to restart the quiz 
+		// restart quiz;
+		function restartQuiz() {
+			$("#js-summary").on("click", "button", event => {
+				event.preventDefault();
+				freshStart();
+				displayIntro();
+			});
+		  };
+		//Clear the counters
+		function freshStart(){
+			score = 0;
+			questionNum = 1;
+			index = -1;
+			$(".js-score").text(`${score}`);
+			$(".js-question-num").text(`${questionNum}`);
 
-		// call the function to handle the quiz // restartQuiz();
-		// start the quiz
-		// render questions
-		// handle the selected option
-		// restart quiz
-
+		}
+		//start quiz
 		function startQuiz() {
 			displayIntro();
-			displayQuestion();
 			submitAnswer();
-		  }
-		  
-		  $(startQuiz);
+			restartQuiz();
+		};
+
+		$(startQuiz);
 		  
